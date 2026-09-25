a51ecf26d6a90716b1509ae796ef2f6421aea5f6
oto <otonebieridze123@gmail.com>
2026-09-25 13:54:17 +0400

    fix: allow full conventional commit type list in commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,23 @@
 export default {
   rules: {
-    'type-enum': [2, 'always', ['init', 'feat', 'fix', 'refactor', 'docs']],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'init',
+        'feat',
+        'fix',
+        'refactor',
+        'docs',
+        'style',
+        'test',
+        'perf',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
     'type-case': [2, 'always', 'lower-case'],
     'subject-empty': [2, 'never'],
     'header-max-length': [2, 'always', 100],
