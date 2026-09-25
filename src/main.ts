15be7cbaa424ead878c3f459cf571b39f8467550
oto <otonebieridze123@gmail.com>
2026-09-25 13:04:23 +0400

    feat: add library page route and empty shell

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createRouter } from './app/router';
 import { renderHeader } from './components/header/header';
 import { renderFooter } from './components/footer/footer';
 import { renderHomePage } from './pages/home/home-page';
+import { renderLibraryPage } from './pages/library/library-page';
 
 const root = document.createElement('div');
 root.id = 'app';
@@ -12,4 +13,7 @@ const main = document.createElement('main');
 root.append(renderHeader(), main, renderFooter());
 document.body.append(root);
 
-createRouter(main, [{ path: '/', render: renderHomePage }]);
+createRouter(main, [
+  { path: '/', render: renderHomePage },
+  { path: '/library', render: renderLibraryPage },
+]);
