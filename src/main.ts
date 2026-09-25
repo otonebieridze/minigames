0b0eed5e78f83ceac5c334d0c72fa2b735e8fd77
oto <otonebieridze123@gmail.com>
2026-09-25 12:59:56 +0400

    refactor: render header and footer once in main instead of per page

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import './styles/globals.scss';
 import { createRouter } from './app/router';
+import { renderHeader } from './components/header/header';
+import { renderFooter } from './components/footer/footer';
 import { renderHomePage } from './pages/home/home-page';
 
 const root = document.createElement('div');
 root.id = 'app';
+
+const main = document.createElement('main');
+
+root.append(renderHeader(), main, renderFooter());
 document.body.append(root);
 
-createRouter(root, [{ path: '/', render: renderHomePage }]);
+createRouter(main, [{ path: '/', render: renderHomePage }]);
